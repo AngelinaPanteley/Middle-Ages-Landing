432cf75638732a9627f7a2a23592035e0caa3380
AngelinaPanteley <panteley-00@mail.ru>
2017-12-15 14:55:58 +0300

    add screenshots and manual

--- scripts/flexslider.js
+++ scripts/flexslider.js
@@ -1,12 +1,7 @@
 // Can also be used with $(document).ready()
 $(window).load(function() {
-  $('.js-flexslider-1').flexslider({
+  $('.flexslider').flexslider({
     animation: "slide",
     controlNav: "thumbnails"
-  });
-
-  $('.js-flexslider-2').flexslider({
-    animation: "slide",
-    directionNav: false
   });
 });
